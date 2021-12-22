c877ecafbcf2168fc90ec10d29e0c02abaf59080
Joseph Fabián Basto Cuadros <79776204+ElJofator@users.noreply.github.com>
2021-12-22 14:49:12 -0500

    Update ServicioPersona.java

--- src/main/java/com/example/Docker/ServicioPersona.java
+++ src/main/java/com/example/Docker/ServicioPersona.java
@@ -1,14 +1,8 @@
 package com.example.Docker;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.ArrayList;
 
-@Entity
-@Table(name="Persona")
 public interface ServicioPersona{
-    
+    ArrayList<Persona> listar();
+    Persona buscarPersona(int id);
 }
