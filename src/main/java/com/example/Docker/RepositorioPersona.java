88840f8d963c49cf248dc1c8de3978839f30da6e
Joseph Fabián Basto Cuadros <79776204+ElJofator@users.noreply.github.com>
2021-12-22 16:24:03 -0500

    añadir cosas

--- src/main/java/com/example/Docker/RepositorioPersona.java
+++ src/main/java/com/example/Docker/RepositorioPersona.java
@@ -1,6 +1,8 @@
 package com.example.Docker;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositorioPersona extends CrudRepository<Persona, Integer>{
 }
